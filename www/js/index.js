f1f4c0ae6912e35e7255de86b0fbcfa62ee0d375
GinoCubeddu <cubg1_14@uni.worc.ac.uk>
2017-11-19 13:55:30 +0000

    add random function

--- www/js/index.js
+++ www/js/index.js
@@ -2,4 +2,8 @@ $("document").ready(function() {
   $('#clickMe').on('click', function() {
     alert("Ouch! You clicked me!");
   });
+
+  function random() {
+    return !Math.round(Math.random());
+  }
 });
